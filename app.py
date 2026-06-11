fd616a35f17a240aa3fa1d532daee324f9765611
peohhh <pyahhhmr@gmail.com>
2026-06-11 20:48:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+st.set_page_config(page_title="ChicSync Dashboard")
+
+st.title("🍓 ChicSync Dashboard 🍵")
+st.write("Distributed Fashion Stock Synchronization System")
+
+st.success("Dashboard is running successfully!")
